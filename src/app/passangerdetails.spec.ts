e43e819b163ed9756e7de06bbe57568c0e90010e
SAI AGARWAL <87272560+Saiagarwal@users.noreply.github.com>
2023-03-30 00:10:34 +0530

    Delete src directory

--- src/app/passangerdetails.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Passangerdetails } from './passangerdetails';
-
-describe('Passangerdetails', () => {
-  it('should create an instance', () => {
-    expect(new Passangerdetails()).toBeTruthy();
-  });
-});
